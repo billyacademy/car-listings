316b53bff6bfbf5aed599362f3d8e55ce1ad8eed
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-02 10:27:21 -0500

    Testing and Functionality done for creating a Manufacturer.

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,5 +1,25 @@
 class ManufacturersController < ApplicationController
   def index
+    @manufacturer = Manufacturer.new
+  end
 
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    if @manufacturer.save
+      redirect_to manufacturer_path(@manufacturer)
+      flash[:notice] = "Manufacturer successfully created."
+    else
+      render :manufacturer
+    end
+  end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country)
   end
 end
