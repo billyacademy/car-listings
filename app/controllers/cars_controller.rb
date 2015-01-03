e61d31248c562e364fef6a1875238b618b6f1769
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-03 15:03:44 -0500

    updated cars model, controller & views

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,14 +1,20 @@
 class CarsController < ApplicationController
     def index
+      @cars = Car.all
     end
 
     def new
       @car = Car.new
+      @manufacturers = Manufacturer.all.collect { |manufacturer| [manufacturer.name, manufacturer.id]}
     end
 
     def create
       @cars = Car.all
       @car = Car.new(car_params)
+      @manufacturers = Manufacturer.all.collect { |manufacturer| [manufacturer.name, manufacturer.id]}
+      car_manufacturer = Manufacturer.find(params[:car]["manufacturer_id"])
+      @car.manufacturer_id = car_manufacturer.id
+
       if @car.save
         redirect_to cars_path
         flash[:notice] = "Car successfully created."
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,8 @@
 class Car < ActiveRecord::Base
   validates :manufacturer_id, presence: true
-  validates :color, presence: true
+  validates :color, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :mileage, presence: true
+  validates_numericality_of :mileage, greater_than_or_equal_to: 0
   validates :year, presence: true, length: { is: 4 }
   validates_numericality_of :year, greater_than: 1919
 
